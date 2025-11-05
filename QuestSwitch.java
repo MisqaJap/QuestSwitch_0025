c7432e9a1103c017260a1964fb972395552cbe18
MisqaJap <ofushgaming@gmail.com>
2025-11-05 12:23:47 +0700

    membuat user input

--- QuestSwitch.java
+++ QuestSwitch.java
@@ -6,6 +6,21 @@ public class QuestSwitch {
         double strukturData, pemrogramanBerorientasiObjek, basisData, adp, rpl;
         double rataRata100, ipk;
         String predikatNilai;
+
+        System.out.println("Welcome to system program seleksi asisten dosen");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Silakan masukkan nilai (0-100) untuk 5 mata kuliah:");
+        
+        System.out.print("Masukkan nilai Struktur Data: ");
+        strukturData = ns.nextDouble();
+        System.out.print("Masukkan nilai Pemrograman Berorientasi Objek: ");
+        pemrogramanBerorientasiObjek = ns.nextDouble();
+        System.out.print("Masukkan nilai Basis Data: ");
+        basisData = ns.nextDouble();
+        System.out.print("Masukkan nilai Algoritma dan Pemrograman: ");
+        adp = ns.nextDouble();
+        System.out.print("Masukkan nilai Rekayasa Perangkat Lunak: ");
+        rpl = ns.nextDouble();
     }
 }
 
