72e985891d2ba3eccde11a2b4819dc2247f0b22b
MisqaJap <ofushgaming@gmail.com>
2025-11-05 11:53:38 +0700

    deklarasi data

--- QuestSwitch.java
+++ QuestSwitch.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class QuestSwitch {
     public static void main(String[] args) {
         Scanner ns = new Scanner(System.in);
+        double strukturData, pemrogramanBerorientasiObjek, basisData, adp, rpl;
+        String predikatNilai;
     }
 }
 
