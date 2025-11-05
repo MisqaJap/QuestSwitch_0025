7d064433800cef5eea9836b0c33db9edc71fda40
MisqaJap <ofushgaming@gmail.com>
2025-11-05 12:21:32 +0700

    menambahkan variable rata rata dan ipk

--- QuestSwitch.java
+++ QuestSwitch.java
@@ -4,6 +4,7 @@ public class QuestSwitch {
     public static void main(String[] args) {
         Scanner ns = new Scanner(System.in);
         double strukturData, pemrogramanBerorientasiObjek, basisData, adp, rpl;
+        double rataRata100, ipk;
         String predikatNilai;
     }
 }
